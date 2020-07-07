c9a085bccf09b34d1967fd72848b20dd06405f43
akito-watanabe211 <67305261+akito-watanabe211@users.noreply.github.com>
2020-07-07 11:49:48 +0900

    Update spear.c

--- src/spear.c
+++ src/spear.c
@@ -2,16 +2,28 @@
 
 int n;
 int k;
-int A[100000];
+int A[100000]; 
 
-
+int yarinonagasa(int a){
+  int sum = 0;
+  for(int i = 0; i < n; i++) sum += A[i] / a;
+  return sum < k;
+}
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
+lb = 0;
+ub =A[n-1];
+while(ub - lb >1){
+  int mid = (ub + lb) / 2;
+  if(yarinonagasa(mid)) ub = mid;
+  else lb = mid;
+}
 
-
+  printf("%d\n",lb);
   return 0;
 }
+
