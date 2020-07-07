6e83b9ae5421b563f004452b00d1b80ea22fe83c
akito-watanabe211 <67305261+akito-watanabe211@users.noreply.github.com>
2020-07-07 11:47:13 +0900

    Update apple.c

--- src/apple.c
+++ src/apple.c
@@ -11,7 +11,30 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
+ lb = -1;
+ ub = n-1;
 
-
-  return 0;
+while(ub - lb > 1){
+int mid = (lb + ub) / 2;
+  if(applebag(mid)) 
+  	ub = mid;
+  else 
+  	lb = mid;
 }
+
+ printf("%d\n",A[ub]);
+
+ return 0;
+}
+
+int applebag(int a){
+int b =0;
+
+for(int i=0; i<n; i++){
+printf("%d" ,A[i]);
+if(A[i]%A[a]==0)
+b += A[i]/A[a];
+else
+b +=A[i]/A[a]+1;
+}
+return b <=k;
